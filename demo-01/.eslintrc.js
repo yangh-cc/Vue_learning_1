3d6627fd8fe545103b3ab2d3b216f0ab01c7e8eb
yangh-cc <2698565024@qq.com>
2025-09-05 09:46:23 +0800

    feat-day04初始化

--- demo-01/.eslintrc.js
+++ demo-01/.eslintrc.js
@@ -1,12 +1,17 @@
 module.exports = {
+  rules: {
+    off: "off",
+  },
   // ...其他配置
   parserOptions: {
     ecmaFeatures: {
-      jsx: true // 启用JSX解析
-    }
+      jsx: true, // 启用JSX解析
+    },
+    sourceType: "module", // 支持 ES 模块
+    ecmaVersion: 2020,
   },
   plugins: [
     // 如果需要可以添加相关插件
     // "react"
-  ]
+  ],
 };
