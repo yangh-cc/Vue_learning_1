fc5f856d355c59e565e74bc38dce7f109b8f98c4
yangh-cc <2698565024@qq.com>
2025-09-08 12:18:11 +0800

    feat： day07 初始化

---
+++ demo-03/src/main.js
@@ -0,0 +1,45 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
+
+const number = 10
+const num = 10
+
+const add = ()=>{
+  return 
+}
+const add2 = ()=>{
+  return
+}
+const add3 = ()=>{
+  return 
+}
+const add4 = ()=>{
+
+}
+const add5 = ()=>{
+  return
+}
+const add6 = ()=>{
+  return
+}
+const add7 =()=>{
+  return 
+}
+const add8 = ()=>{
+  rreturn 
+}
+const add9 =()=>{
+  return 
+}
+const add10 = () =>{
+  return 
+}
+
