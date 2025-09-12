559ee566152fc788d1f66303a4ebf382ee528927
yangh-cc <2698565024@qq.com>
2025-09-12 20:00:15 +0800

    feat：到父传子，子传父

---
+++ vue3-demo/src/App.vue
@@ -0,0 +1,92 @@
+<script setup>
+
+// export default {
+//   setup () {
+//     //数据
+//     const message = 'hello Vue3'
+//     //函数
+//     const logMessage = () => {
+//       console.log(message);
+//     }
+    
+//     return {
+//       message,
+//       logMessage
+//     }
+//   }
+// }
+
+
+//  const message = 'hello Vue3'
+//     // 函数
+//     const logMessage = () => {
+//       console.log(message);
+//     }
+// import {ref ,computed} from 'vue'
+// // const count = ref(0)
+// // const setCount =() => {
+// //   count.value++
+// // }
+// const count = ref(1)
+// const plusOne = computed({
+//   get: () => count.value+1,
+//   set:(val)=>{
+//     count.value = val-1
+//   }
+// })
+// plusOne.value = 1
+// console.log(count.value);
+
+//父传子
+import {ref} from 'vue'
+import SonCom from '@/components/son-com.vue'
+const money =ref(100)
+const getMoney = () => {
+  money.value += 10
+}
+
+const changeFn = (newMony)=>{
+  money.value=newMony
+}
+</script>
+
+<template>
+  <!-- <div>{{ count }}</div>
+  <button @click="setCount">按钮</button> -->
+
+  <div>
+    <h3>父组件-{{ money }}
+      <button @click="getMoney">挣钱</button>
+    </h3>
+    <SonCom @changeMoney="changeFn" car="宝马车" :money="money"></SonCom>
+  </div>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
---
+++ vue3-demo/src/components/son-com.vue
@@ -0,0 +1,27 @@
+<script setup>
+//子组件
+const props = defineProps({
+    car:String,
+    money:Number
+})
+const emit =defineEmits(['changeMoney'])
+console.log(props.car);
+console.log(props.money);
+
+const buy= ()=>{
+    emit('changeMoney',5)
+}
+
+</script>
+
+<template>
+    <div class="son">我是子组件-{{ car }}-{{ money }} <button @click="buy">花钱</button></div>
+
+</template>
+
+<style>
+.son{
+    border: 1px solid #000;
+    padding: 30px;
+}
+</style>
